9e0ec17b60c52b1e35b6f2d74f6de8b8e202535b
Vikas00kr <vikasvrm00@gmail.com>
2019-08-31 11:33:43 +0530

    preview

---
+++ precache-manifest.33a11f5ac7cc879254ec18c97354c587.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4500139013eb68afc75ceadff09d8003",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4110a16200ba56c42e9b",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d9548050a8301753a7ec",
+    "url": "/static/css/main.856b9f04.chunk.css"
+  },
+  {
+    "revision": "4110a16200ba56c42e9b",
+    "url": "/static/js/2.4eeb8105.chunk.js"
+  },
+  {
+    "revision": "d9548050a8301753a7ec",
+    "url": "/static/js/main.83372e16.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
